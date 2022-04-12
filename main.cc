4aaf86884c1f55fe5f3ed0d476c86286ceded0b0
Oliver Portee <oliver.portee@gmail.com>
2022-04-12 14:45:08 +0200

    small test program for keyboard delay

--- main.cc
+++ main.cc
@@ -6,7 +6,7 @@ int main()
 
 	CGA_Screen screen{};
 	Keyboard_Controller ctrl{};
-	ctrl.set_repeat_rate(5, 3);
+	ctrl.set_repeat_rate(5, 0);
 	ctrl.set_led(Keyboard_Controller::led::caps_lock, true);
 	ctrl.set_led(Keyboard_Controller::led::num_lock, true);
 	screen.print("done", 4, 0x0f);
@@ -14,7 +14,24 @@ int main()
 		Key key = ctrl.key_hit();
 		if (key.valid()) {
 			char c = (char) key;
+			switch (c)
+			{
+			case '0':
+				ctrl.set_repeat_rate(5, 0);
+				break;
+			case '1':
+				ctrl.set_repeat_rate(5, 1);
+				break;
+			case '2':
+				ctrl.set_repeat_rate(5, 2);
+				break;
+			case '3':
+				ctrl.set_repeat_rate(5, 3);
+				break;
+			default:
 			screen.print(&c, 1, 0x0f);
+				break;
+			}
 		}
 	}
 
