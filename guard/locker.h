68527dff4c39be6968fa7e0f82d68c8f2382cf50
Mark Überall <mark.ueberall@mailbox.tu-dresden.de>
2022-05-04 12:03:55 +0200

    add locker methods

--- guard/locker.h
+++ guard/locker.h
@@ -14,10 +14,45 @@
 #ifndef __Locker_include__
 #define __Locker_include__
 
+#include "machine/cpu.h"
+#include "device/cgastr.h"
+
+extern CPU cpu;
+extern CGA_Stream kout;
+
 class Locker {
 private:
 	Locker(const Locker &copy); // prevent copying
+	bool available;
 /* Add your code here */ 
+public: 
+	Locker() {
+		available = true;
+	}
+
+	inline void enter() {
+		if (!available) {
+			// print error and halt
+			kout << "ERROR: multiple calls to 'enter()'!" << flush;
+			cpu.halt();
+		}
+
+		available = false;
+	}
+
+	inline void retne() {
+		if (available) {
+			// print error and halt
+			kout << "ERROR: multiple calls to 'retne()'!" << flush;
+			cpu.halt();
+		}
+
+		available = true;
+	}
+
+	inline bool avail() {
+		return available;
+	}
 };
 
 #endif
