c6e536dc1267749f01985ab0eae09c6d56b6f73b
Oliver Portee <oliver.portee@gmail.com>
2022-06-06 16:56:44 +0200

    make task1 and task2 print at specific coordinates

--- user/task1.cc
+++ user/task1.cc
@@ -2,6 +2,7 @@
 #include "task1.h"
 
 #include "device/cgastr.h"
+#include "guard/secure.h"
 
 char stack1[65536];
 
@@ -10,6 +11,8 @@ Task1::Task1() : Thread{stack1 + sizeof(stack1)} {}
 void Task1::action() {
     unsigned int i = 0;
     while (true) {
+        Secure secure;
+        kout.setpos(0, 0);
         kout << "task 1: " << i++ << endl;
     }
 }
--- user/task2.cc
+++ user/task2.cc
@@ -2,6 +2,7 @@
 #include "task2.h"
 
 #include "device/cgastr.h"
+#include "guard/secure.h"
 
 char stack2[65536];
 
@@ -10,6 +11,8 @@ Task2::Task2() : Thread{stack2 + sizeof(stack2)} {}
 void Task2::action() {
     unsigned int i = 0;
     while (true) {
+        Secure secure;
+        kout.setpos(0, 10);
         kout << "task 2: " << i++ << endl;
     }
 }
