e279d64231c03b2ef60bb7497a9b671b4a0cc04e
Oliver Portee <oliver.portee@gmail.com>
2022-06-06 17:21:39 +0200

    put main() into protected mode

--- main.cc
+++ main.cc
@@ -1,17 +1,18 @@
 #include "device/keyboard.h"
 #include "device/watch.h"
+#include "guard/secure.h"
 #include "machine/cpu.h"
 #include "syscall/guarded_scheduler.h"
 #include "user/appl.h"
 
 int main() {
+    Secure secure;
     cpu.enable_int();
     keyboard.plugin();
-
-    scheduler.ready(app);
     watch.windup();
 
-    while (true) {}
+    scheduler.Scheduler::ready(app);
+    scheduler.Scheduler::schedule();
 
     return 0;
 }
