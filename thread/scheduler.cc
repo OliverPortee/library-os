8127df8a91815a19ba5b892f396d786fb38dcd59
Oliver Portee <oliver.portee@gmail.com>
2022-06-06 08:05:11 +0200

    cosmetics

--- thread/scheduler.cc
+++ thread/scheduler.cc
@@ -9,46 +9,42 @@
 /*****************************************************************************/
 
 #include "thread/scheduler.h"
+
 #include "guard/secure.h"
 
 Scheduler::Scheduler() {}
 
-void Scheduler::ready(Entrant& that) {
-    Secure secure;
-    queue.enqueue(&that);
-}
+void Scheduler::ready(Entrant& that) { queue.enqueue(&that); }
 
 void Scheduler::schedule() {
-    // TODO: Secure?
     Chain* chain = queue.dequeue();
-    if (chain == nullptr) cpu.halt();       // halt machine if no more tasks to schedule
+    if (chain == nullptr) {
+        // halt machine if no more tasks to schedule
+        cpu.halt();
+    }
     Entrant* e = static_cast<Entrant*>(chain);
     Coroutine* coroutine = static_cast<Coroutine*>(e);
 
-    if(active() == nullptr) {
+    if (active() == nullptr) {
         go(*coroutine);
     } else {
         dispatch(*coroutine);
     }
 }
 
-void Scheduler::exit() {
-    schedule(); // doesn't make much sense to have a separate method for that
-}
+void Scheduler::exit() { schedule(); }
 
 void Scheduler::kill(Entrant& that) {
-    Chain* chainp = static_cast<Chain*>(&that); // is &that correct?
-    
+    Chain* chainp = static_cast<Chain*>(&that);
+
     if (active() == &that) {
         exit();
     } else {
-        Secure secure;
         queue.remove(chainp);
     }
 }
 
 void Scheduler::resume() {
-    // TODO: Secure?
     if (active() != nullptr) {
         Entrant* e = static_cast<Entrant*>(active());
         Chain* c = static_cast<Chain*>(e);
