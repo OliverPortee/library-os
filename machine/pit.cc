47941340196aadd37577cc7acf6de2b95ef81895
Oliver Portee <oliver.portee@gmail.com>
2022-06-13 09:34:55 +0200

    add comment for pit ctrl byte

--- machine/pit.cc
+++ machine/pit.cc
@@ -18,6 +18,10 @@ int PIT::interval() const { return us; }
 void PIT::interval(int us) {
     assert(us >= 1, "PIT::interval called with < 1 us");
     const unsigned int counter = us / 0.838;
+    //  00 – selects counter 0
+    //  11 – read both lower order and higher order bytes
+    // 010 – periodic interrupts
+    //   0 – binary counting
     const unsigned char ctrl_byte = 0b00110100;
     port.outb(ctrl_byte);
     port.outb(counter & 0xff); // lower order
