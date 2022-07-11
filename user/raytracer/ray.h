7b8cba623dfb0f3823c68a5f264824100b054eb0
Oliver Portee <oliver.portee@gmail.com>
2022-07-11 09:42:50 +0200

    ray simplification

--- user/raytracer/ray.h
+++ user/raytracer/ray.h
@@ -3,17 +3,11 @@
 
 #include "vec3.h"
 
-class Ray {
-    public: 
-        Point3 origin;
-        Point3 direction;
-    
-    Point3 origin() const { return origin; }
-    Vec3 direction() const { return direction; }
+struct Ray {
+    Point3 origin;
+    Point3 direction;
 
-    Point3 at(double t) const { 
-        return origin + t * direction;
-    }
+    Point3 at(double t) const { return origin + t * direction; }
 };
 
 #endif
