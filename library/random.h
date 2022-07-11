68d4f5126992592c0332103a8e731b68c68d6941
Oliver Portee <oliver.portee@gmail.com>
2022-07-11 10:51:07 +0200

    random double functions

--- library/random.h
+++ library/random.h
@@ -23,7 +23,10 @@ public:
 	Random(int seed);
 
 	// NUMBER: Returns a random number
-	int number();
+	int random_int();
+
+	double random_double();
+	double random_double(double min_incl, double max_excl);
 };
 
 #endif
